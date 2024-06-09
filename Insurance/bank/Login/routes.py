2dd62dc1845346a431c4b35ad057dcbbbc535044
NicolaiEVinther <pingoynwa@gmail.com>
2024-06-09 21:37:37 +0200

    added ER diagram

--- Insurance/bank/Login/routes.py
+++ Insurance/bank/Login/routes.py
@@ -103,51 +103,6 @@ def about():
     print(mysession)
     return render_template('about.html', title='About', current_page='about')
 
-@Login.route("/direct", methods=['GET', 'POST'])
-def direct():
-    mysession["state"] = "direct"
-    print("L1", mysession)
-    role = None
-
-    if current_user.is_authenticated:
-        return redirect(url_for('Login.home'))
-
-    is_employee = True if request.args.get('is_employee') == 'true' else False
-    form = DirectCustomerLoginForm()
-
-    if form.validate_on_submit():
-        user = select_customer_direct(form.p.data)
-        print("L2 user", user)
-
-        if user is not None:
-            print("L3 role:" + user.role)
-            mysession["role"] = roles[2]  # customer
-            mysession["id"] = form.p.data
-            print("L3", mysession)
-            print("L3", roles)
-
-            login_user(user, remember=form.remember.data)
-            flash('Login successful.', 'success')
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('Login.home'))
-        else:
-            flash('Login Unsuccessful. Please check identifier and password', 'danger')
-
-    direct_users = select_customers_direct()
-    print("L2 direct", direct_users)
-
-    teachers = [{"id": str(6234), "name": "anders. teachers with 6."}, {"id": str(6214), "name": "simon"},
-                {"id": str(6862), "name": "dmitry"}, {"id": str(6476), "name": "finn"}]
-    parents = [{"id": str(4234), "name": "parent-anders. parents with 4.", "address": "address 1"},
-               {"id": str(5002), "name": "parent-simon", "address": "address 2"},
-               {"id": str(4862), "name": "parent-dmitry", "address": "address 3"},
-               {"id": str(5010), "name": "parent-finn", "address": "address 4"}]
-    students = [{"id": str(5002), "name": "student-anders. students with 5."}, {"id": str(5214), "name": "student-simon"},
-                {"id": str(5010), "name": "student-dmitry"}, {"id": str(5476), "name": "student-finn"}]
-
-    return render_template('direct.html', title='Direct Login', is_employee=is_employee, form=form,
-                           students=students, radio_direct=direct_users, role=role, current_page='direct')
-
 @Login.route("/login", methods=['GET', 'POST'])
 def login():
     session["state"] = "login"
